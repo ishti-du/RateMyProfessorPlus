e65a7800de194120c230768db0580ecad11705d5
hschafer370 <90478800+hschafer370@users.noreply.github.com>
2021-10-20 12:39:02 -0400

    the last commit deleted everything but now it is fixed

--- rmp_bd_app/urls.py
+++ rmp_bd_app/urls.py
@@ -11,16 +11,15 @@ urlpatterns = [
     path('', views.index, name='index'),
     # University
     path('university/', views.universities, name='universities'),
-    path('university/<int:university_id>', views.university, name='university'),
-    path('departments/<int:university_id>', views.departments, name='departments'),
+    path('university/<int:university_id>/departments', views.departments, name='departments'),
     # Page for adding a new University
     path('new_university/', views.new_university, name='new_university'),
     # Page for faculty listing
-    path('professors/<int:department_id>', views.professor, name='professor'),
-    path('professor_details/<int:faculty_id>', views.professor_details, name='professor_details'),
+    path('professor/<int:department_id>', views.professor, name='professor'),
+    path('professor_details/<int:professor_id>', views.professor_details, name='professor_details'),
     path('new_department/', views.new_department, name='new_department'),
     # Page for adding a new faculty
     path('new_faculty/', views.new_faculty, name='new_faculty'),
     # Page for adding a new feedback
-    path('new_feedback/', views.new_feedback, name='new_feedback')
+    path('new_feedback/<int:professor_id>', views.new_feedback, name='new_feedback')
     ]
--- rmp_bd_app/views.py
+++ rmp_bd_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 
-from .models import University, Department, Faculty, Professor
-from .forms import UniversityForm, DepartmentForm, FacultyForm, FeedbackForm
+from .models import University, Department, Professor
+from .forms import UniversityForm, DepartmentForm, FeedbackForm, ProfessorForm
+
 
 # Create your views here.
 def index(request):
@@ -14,10 +15,10 @@ def universities(request):
     context = {'universities': universities}
     return render(request, 'rmp_bd_app/universities.html', context)
 
-def departments(request):
-    """The departments page for RMP BD"""
-    departments = Department.objects.order_by('date_added')
-    context = {'Departments': departments}
+def departments(request, university_id):
+    university = University.objects.get(id=university_id)
+    departments = Department.objects.filter(university=University.objects.get(id=university_id)).order_by('date_added')
+    context = {'departments': departments, 'university': university}
     return render(request, 'rmp_bd_app/departments.html', context)
 
 def university(request, university_id):
@@ -25,20 +26,20 @@ def university(request, university_id):
     university = University.objects.get(id=university_id)
     departments = university.department_set.order_by('-date_added')
     context = {'university' : university, 'departments' : departments}
-    return render(request, 'rmp_bd_app/departments.html', context)
+    return render(request, 'rmp_bd_app/universities.html', context)
 
 def professor(request, department_id):
     """Shows faculty members for a department"""
     department = Department.objects.get(id=department_id)
-    professor = department.faculty_set.order_by('-date_added')
-    context = {'department': department, 'faculties': professor}
-    return render(request, 'rmp_bd_app/professsors.html', context)
+    professor = Professor.objects.filter(department=Department.objects.get(id=department_id)).order_by('date_added')
+    context = {'department': department, 'professor': professor}
+    return render(request, 'rmp_bd_app/professors.html', context)
 
-def professor_details(request, faculty_id):
+def professor_details(request, professor_id):
     """Shows the students' feedback about a faculty"""
-    professor = Professor.objects.get(id=faculty_id)
+    professor = Professor.objects.get(id=professor_id)
     #feedback = faculty.feedback_set.order_by('-date_added')
-    context = {'Professor': professor,}
+    context = {'professor': professor}
     return render(request, 'rmp_bd_app/professor_details.html', context)
 
 def new_university(request):
@@ -77,10 +78,10 @@ def new_faculty(request):
     """Add a new Faculty"""
     if request.method != 'POST':
         # no data submitted, create a blank forms
-        form = FacultyForm()
+        form = ProfessorForm()
     else:
         # POST data submitted; process date_added
-        form = FacultyForm(data=request.POST)
+        form = ProfessorForm(data=request.POST)
         if form.is_valid():
             form.save()
             return redirect('rmp_bd_app:universities')
@@ -90,8 +91,8 @@ def new_faculty(request):
     return render(request, 'rmp_bd_app/new_faculty.html', context)
 
 
-def new_feedback(request, faculty_id):
-    professor = Professor.objects.get(id=faculty_id)
+def new_feedback(request, professor_id):
+    professor = Professor.objects.get(id=professor_id)
     if request.method != 'POST':
         # no data submitted, create a blank forms
         form = FeedbackForm()
@@ -103,7 +104,7 @@ def new_feedback(request, faculty_id):
             return redirect('rmp_bd_app:universities')
 
     # Display a blank or invalid form
-    context = {'form': form, 'Professor': professor}
+    context = {'form': form, 'professor': professor}
     return render(request, 'rmp_bd_app/reviewform.html', context)
 
     
