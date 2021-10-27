ae99d478ace2e55f3444c37bfe008b40daf510b5
Jason Ayling <82242592+wereallsquidwrd@users.noreply.github.com>
2021-10-27 11:57:43 -0400

    adding files

---
+++ admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Country
+from .models import University
+from .models import Campus
+from .models import Department
+from .models import Professor
+from .models import Campus_Professor
+from .models import Course
+from .models import Prereq
+from .models import Professor_Course
+from .models import Review
+from .models import Tag
+from .models import Review_Tag
+from .models import Student_Profile
+from .models import Professor_Profile
+
+# from .models import Faculty
+# from .models import Feedback
+
+admin.site.register(Country)
+admin.site.register(University)
+admin.site.register(Campus)
+admin.site.register(Department)
+admin.site.register(Professor)
+admin.site.register(Campus_Professor)
+admin.site.register(Course)
+admin.site.register(Prereq)
+admin.site.register(Professor_Course)
+admin.site.register(Review)
+admin.site.register(Tag)
+admin.site.register(Review_Tag)
+admin.site.register(Student_Profile)
+admin.site.register(Professor_Profile)
+
+# admin.site.register(Faculty)
+# admin.site.register(Feedback)
---
+++ forms.py
@@ -0,0 +1,55 @@
+from django import forms
+
+
+from .models import University, Department, Professor, Feedback, Student_Profile, Professor_Profile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+
+class UniversityForm(forms.ModelForm):
+    class Meta:
+        model = University
+        fields = ['country', 'university_name']
+        labels = {'text': ''}
+
+
+class DepartmentForm(forms.ModelForm):
+    class Meta:
+        model = Department
+        fields = ['university', 'department_name']
+        labels = {'text': ''}
+
+
+class ProfessorForm(forms.ModelForm):
+    class Meta:
+        model = Professor
+        fields = ['current_university', 'campus', 'department', 'honorific', 'first_name', 'last_name']
+        labels = {'text': ''}
+
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['faculty', 'feedback']
+        labels = {'text': ''}
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
+
+
+class StudentProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Student_Profile
+        fields = ['school_name']
+
+
+class ProfessorProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Professor_Profile
+        fields = ['faculty_directory_url', 'faculty_phone_number']
