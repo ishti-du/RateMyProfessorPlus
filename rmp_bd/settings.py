a09f891d0003025052ff8914bab9a44f0320a29e
Jason Ayling <82242592+wereallsquidwrd@users.noreply.github.com>
2021-10-27 11:59:24 -0400

    adding files

--- rmp_bd/settings.py
+++ rmp_bd/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'rmp_bd_app.processor_file_name.user',
+)
