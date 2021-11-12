425027cba0b91241235046cf84524111a086c0ec
kavanAdeshara <kavanadeshara1@gmail.com>
2021-11-12 11:49:15 -0500

    resolving code after migrations

--- rmp_bd_app/admin.py
+++ rmp_bd_app/admin.py
@@ -8,12 +8,12 @@ from .models import Professor
 from .models import UniversityProfessor
 from .models import Course
 from .models import Prereq
-from .models import Professor_Course
+from .models import ProfessorCourse
 from .models import Review
 from .models import Tag
-from .models import Review_Tag
-from .models import Student_Profile
-from .models import Professor_Profile
+from .models import ReviewTag
+from .models import StudentProfile
+from .models import ProfessorProfile
 
 
 admin.site.register(University)
@@ -23,12 +23,12 @@ admin.site.register(Professor)
 admin.site.register(UniversityProfessor)
 admin.site.register(Course)
 admin.site.register(Prereq)
-admin.site.register(Professor_Course)
+admin.site.register(ProfessorCourse)
 admin.site.register(Review)
 admin.site.register(Tag)
-admin.site.register(Review_Tag)
-admin.site.register(Student_Profile)
-admin.site.register(Professor_Profile)
+admin.site.register(ReviewTag)
+admin.site.register(StudentProfile)
+admin.site.register(ProfessorProfile)
 
 # admin.site.register(Faculty)
 # admin.site.register(Feedback)
