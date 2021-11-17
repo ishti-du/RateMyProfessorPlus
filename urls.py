b61114bf34e919c443a7c25114f455a6d9a823fd
Jason Ayling <82242592+wereallsquidwrd@users.noreply.github.com>
2021-11-17 10:02:40 -0500

    review deletion code added

---
+++ urls.py
@@ -0,0 +1,39 @@
+"""Defines URL patterns for rmp_bd_app"""
+
+from django.urls import path
+
+from . import views
+
+
+
+app_name = 'rmp_bd_app'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    # University
+    path('university/', views.universities, name='universities'),
+    path('university/<int:university_id>/departments', views.departments, name='departments'),
+    # Page for adding a new University
+    path('new_university/', views.new_university, name='new_university'),
+    # Page for faculty listing
+    path('professor/<int:department_id>', views.professor, name='professor'),
+    path('professor_details/<int:professor_id>', views.professor_details, name='professor_details'),
+    path('new_department/', views.new_department, name='new_department'),
+    
+    # Page for adding a new professor
+    path('new_professor/', views.new_professor, name='new_professor'),
+    # Page for adding a new review
+    path('new_review/<int:professor_id>', views.new_review, name='new_review'),
+    # Page for student sign up
+    path('student_signup/', views.student_signup_view, name="student_signup"),
+    # Page for professor sign up
+    path('professor_signup/', views.professor_signup_view, name="professor_signup"),
+    # Page for sign in
+    path('login/', views.signin_view, name="signin"),
+    # Page for sign out
+    path('logout/', views.signout_view, name="signout"),
+
+    path('profile/', views.user_profile_view, name="profile"),
+    # Test page for adding a course
+    path('new_course/', views.new_course, name='new_course'),
+    ]
---
+++ views.py
@@ -0,0 +1,213 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+from .models import University, Department, Professor, Course, FlagManager
+from .forms import UniversityForm, DepartmentForm, ProfessorForm, ReviewForm, StudentProfileForm, ProfessorProfileForm, CreateUserForm, CourseForm
+
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+# Create your views here.
+def index(request):
+    """The home page for RMP BD"""
+    print("is authenticated", request.user.is_authenticated, request.user)
+    return render(request, 'rmp_bd_app/index.html', {"user": request.user})
+
+
+def universities(request):
+    """The univeristy page for RMP BD"""
+    universities = University.objects.order_by('date_added')
+    context = {'universities': universities}
+    return render(request, 'rmp_bd_app/universities.html', context)
+
+def departments(request, university_id):
+    university = University.objects.get(id=university_id)
+    departments = Department.objects.filter(university=University.objects.get(id=university_id)).order_by('date_added')
+    context = {'departments': departments, 'university': university}
+    return render(request, 'rmp_bd_app/departments.html', context)
+
+def university(request, university_id):
+    """Shows each individual university """
+    university = University.objects.get(id=university_id)
+    departments = university.department_set.order_by('-date_added')
+    context = {'university' : university, 'departments' : departments}
+    return render(request, 'rmp_bd_app/universities.html', context)
+
+def professor(request, department_id):
+    """Shows professors for a department"""
+    department = Department.objects.get(id=department_id)
+    professor = Professor.objects.filter(department=Department.objects.get(id=department_id)).order_by('date_added')
+    context = {'department': department, 'professor': professor}
+    return render(request, 'rmp_bd_app/professors.html', context)
+
+
+def professor_details(request, professor_id):
+    """Shows the students' reviews about a professor"""
+    professor = Professor.objects.get(id=professor_id)
+    context = {'professor': professor}
+    return render(request, 'rmp_bd_app/professor_details.html', context)
+
+
+def new_university(request):
+    """Add a new University"""
+    if request.method != 'POST':
+        # no data submitted, create a blank forms
+        form = UniversityForm()
+    else:
+        # POST data submitted; process date_added
+        form = UniversityForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rmp_bd_app:universities')
+
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'rmp_bd_app/new_university.html', context)
+
+
+def new_department(request):
+    """Add a new Department"""
+    if request.method != 'POST':
+        # no data submitted, create a blank forms
+        form = DepartmentForm()
+    else:
+        # POST data submitted; process date_added
+        form = DepartmentForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rmp_bd_app:universities')
+
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'rmp_bd_app/new_department.html', context)
+
+def new_professor(request):
+    """Add a new Professor"""
+    if request.method != 'POST':
+        # no data submitted, create a blank forms
+        form = ProfessorForm()
+    else:
+        # POST data submitted; process date_added
+        form = ProfessorForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rmp_bd_app:universities')
+
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'rmp_bd_app/new_professor.html', context)
+
+
+def new_review(request, professor_id):
+    professor = Professor.objects.get(id=professor_id)
+    if request.method != 'POST':
+        # no data submitted, create a blank forms
+        form = ReviewForm()
+    else:
+        # POST data submitted; process date_added
+        form = ReviewForm(data=request.POST)
+        if form.is_valid():
+            dele = FlagManager.get_queryset()
+            dele.delete()
+            form.save()
+            return redirect('rmp_bd_app:universities')
+
+    # Display a blank or invalid form
+    context = {'form': form, 'professor': professor}
+    return render(request, 'rmp_bd_app/reviewform.html', context)
+
+def new_course(request):
+    """Add a new course"""
+    form = CourseForm() 
+    course_query = Course.objects.all()
+    courses = []
+    for c in course_query:
+        courses.append((c.course_number, c.course_title))
+    context = {'form': form, 'courses': courses}
+    if request.method == 'POST':
+        # POST data submitted; process date_added
+        # Redirects back to add course page
+        form = CourseForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rmp_bd_app:new_course', context)
+            
+    # Display a blank or invalid form
+    return render(request, 'rmp_bd_app/new_course.html', context)
+
+# /search/?course=
+# Request made whenever input is made in course number
+# Filters courses based on course number
+def search_course(request):
+    course_number = request.GET.get('course')
+    course_numbers = []
+    if course_number:
+        courses = Course.objects.filter(course_number__icontains=course_number)
+        for course in courses:
+            course_numbers.append((course.course_number, course.course_title))
+    return JsonResponse({'status': 200, 'data' : course_numbers})
+
+def student_signup_view(request):
+    user_form = CreateUserForm(request.POST)
+    student_form = StudentProfileForm(request.POST)
+    if request.method == "POST":
+        if user_form.is_valid() and student_form.is_valid():
+            user = user_form.save()
+            profile = student_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            username = user_form.cleaned_data.get('username')
+            password = user_form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    return render(request, 'rmp_bd_app/student_signup.html', {'user_form': user_form, 'student_form': student_form})
+
+def professor_signup_view(request):
+    user_form = CreateUserForm(request.POST)
+    professor_form = ProfessorProfileForm(request.POST)
+    if request.method == "POST":
+        if user_form.is_valid() and professor_form.is_valid():
+            user = user_form.save()
+            profile = professor_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            username = user_form.cleaned_data.get('username')
+            password = user_form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    return render(request, 'rmp_bd_app/professor_signup.html', {'user_form': user_form, 'professor_form': professor_form})
+
+def signin_view(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        return render(request, 'rmp_bd_app/login.html', {'form': form})
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                print(user)
+                login(request, user)
+                return redirect('/')
+            else:
+                print('User not found')
+        else:
+            # If there were errors, we render the form with these
+            # errors
+            return render(request, 'rmp_bd_app/login.html', {'form': form})
+
+
+def signout_view(request):
+    logout(request)
+    return redirect('/login')
+
+
+def user_profile_view(request):
+    return render(request, 'rmp_bd_app/profile.html')
