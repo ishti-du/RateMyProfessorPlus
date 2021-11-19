2658cf279fb54f39f1fe1ed367ad1e1cd7632be0
Matt C <55063086+MCoutts@users.noreply.github.com>
2021-11-19 12:24:08 -0500

    Merge branch 'master' of https://github.com/ishti-du/RateMyProfessorPlus into ThumbsUpDown-IP#2
    # Conflicts:
    #	rmp_bd_app/models.py
    #	rmp_bd_app/views.py

--- rmp_bd_app/views.py
+++ rmp_bd_app/views.py
@@ -7,9 +7,11 @@ from ipware import get_client_ip
 import ipware  # this package retrieves the clients IP
 from .forms import UniversityForm, DepartmentForm, ProfessorForm, ReviewForm, StudentProfileForm, \
     ProfessorProfileForm, CreateUserForm, CourseForm, UpdateUserForm
-from .models import Course, Department, Professor, Professor_Profile, Student_Profile, University
+from .models import Course, Department, Professor, ProfessorProfile, StudentProfile, University
 
 ''' Func that retrieves the IP address at the start of the website '''
+
+
 def retrieveIP(request):
     '''
     ~~~~~
@@ -22,27 +24,29 @@ def retrieveIP(request):
 
     # if we can't get the IP then we check constraints here
     if ip is None:
-        ip = "0.0.0.0" # set IP as 0.0.0.0 if we can't find it
+        ip = "0.0.0.0"  # set IP as 0.0.0.0 if we can't find it
     else:
         # routable = True or False
         if is_routable:
-            ipv = "Public" # if the ip returns true (not local)
+            ipv = "Public"  # if the ip returns true (not local)
         else:
-            ipv = "Private" # if the ip returns false (local)
+            ipv = "Private"  # if the ip returns false (local)
 
     print(ip)
-    return(ip)
+    return (ip)
+
+
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.generic.list import ListView
 from .models import University, Department, Professor, Course, User, StudentProfile
-from .forms import UniversityForm, DepartmentForm, ProfessorForm, ReviewForm, StudentProfileForm, ProfessorProfileForm, CreateUserForm, CourseForm, UpdateUserForm
+from .forms import UniversityForm, DepartmentForm, ProfessorForm, ReviewForm, StudentProfileForm, ProfessorProfileForm, \
+    CreateUserForm, CourseForm, UpdateUserForm
 from django.db.models import Q
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from ipware import get_client_ip
 from django.contrib.gis.geoip2 import GeoIP2
-
 
 
 # Create your views here.
@@ -58,18 +62,21 @@ def universities(request):
     context = {'universities': universities}
     return render(request, 'rmp_bd_app/universities.html', context)
 
+
 def departments(request, university_id):
     university = University.objects.get(id=university_id)
     departments = Department.objects.filter(university=University.objects.get(id=university_id)).order_by('date_added')
     context = {'departments': departments, 'university': university}
     return render(request, 'rmp_bd_app/departments.html', context)
 
+
 def university(request, university_id):
     """Shows each individual university """
     university = University.objects.get(id=university_id)
     departments = university.department_set.order_by('-date_added')
-    context = {'university' : university, 'departments' : departments}
+    context = {'university': university, 'departments': departments}
     return render(request, 'rmp_bd_app/universities.html', context)
+
 
 def professor(request, department_id):
     """Shows professors for a department"""
@@ -119,6 +126,7 @@ def new_department(request):
     context = {'form': form}
     return render(request, 'rmp_bd_app/new_department.html', context)
 
+
 def new_professor(request):
     """Add a new Professor"""
     if request.method != 'POST':
@@ -152,9 +160,10 @@ def new_review(request, professor_id):
     context = {'form': form, 'professor': professor}
     return render(request, 'rmp_bd_app/reviewform.html', context)
 
+
 def new_course(request):
     """Add a new course"""
-    form = CourseForm() 
+    form = CourseForm()
     course_query = Course.objects.all()
     courses = []
     for c in course_query:
@@ -167,9 +176,10 @@ def new_course(request):
         if form.is_valid():
             form.save()
             return redirect('rmp_bd_app:new_course', context)
-            
+
     # Display a blank or invalid form
     return render(request, 'rmp_bd_app/new_course.html', context)
+
 
 # /search/?course=
 # Request made whenever input is made in course number
@@ -181,19 +191,20 @@ def search_course(request):
         courses = Course.objects.filter(course_number__icontains=course_number)
         for course in courses:
             course_numbers.append((course.course_number, course.course_title))
-    return JsonResponse({'status': 200, 'data' : course_numbers})
+    return JsonResponse({'status': 200, 'data': course_numbers})
+
 
 def student_signup_view(request):
     user_form = CreateUserForm(request.POST)
     student_form = StudentProfileForm(request.POST)
     if request.method == "POST":
         if user_form.is_valid() and student_form.is_valid():
-            ip = retrieveIP(request) # Matt - retrieves the IP as an int
-            ip2 = str(ip) # Matt - converts the IP to a string to be added to the profile
+            ip = retrieveIP(request)  # Matt - retrieves the IP as an int
+            ip2 = str(ip)  # Matt - converts the IP to a string to be added to the profile
             user = user_form.save()
             profile = student_form.save(commit=False)
             profile.user = user
-            profile.ip_address = ip2 # Matt -  adds the IP to the profile created
+            profile.ip_address = ip2  # Matt -  adds the IP to the profile created
             profile.save()
             username = user_form.cleaned_data.get('username')
             password = user_form.cleaned_data.get('password1')
@@ -202,24 +213,27 @@ def student_signup_view(request):
             return redirect('/')
     return render(request, 'rmp_bd_app/student_signup.html', {'user_form': user_form, 'student_form': student_form})
 
+
 def professor_signup_view(request):
     user_form = CreateUserForm(request.POST)
     professor_form = ProfessorProfileForm(request.POST)
     if request.method == "POST":
         if user_form.is_valid() and professor_form.is_valid():
-            ip = retrieveIP(request) # Matt - retrieves the IP as an int
-            ip2 = str(ip) # Matt - converts the IP to a string to be added to the profile
+            ip = retrieveIP(request)  # Matt - retrieves the IP as an int
+            ip2 = str(ip)  # Matt - converts the IP to a string to be added to the profile
             user = user_form.save()
             profile = professor_form.save(commit=False)
             profile.user = user
-            profile.ip_address = ip2 # Matt - adds the IP to the profile created
+            profile.ip_address = ip2  # Matt - adds the IP to the profile created
             profile.save()
             username = user_form.cleaned_data.get('username')
             password = user_form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('/')
-    return render(request, 'rmp_bd_app/professor_signup.html', {'user_form': user_form, 'professor_form': professor_form})
+    return render(request, 'rmp_bd_app/professor_signup.html',
+                  {'user_form': user_form, 'professor_form': professor_form})
+
 
 def signin_view(request):
     if request.user.is_authenticated:
@@ -249,8 +263,11 @@ def signin_view(request):
 def signout_view(request):
     logout(request)
     return redirect('/login')
+
+
 def user_profile_view(request):
     return render(request, 'rmp_bd_app/profile.html')
+
 
 def user_profile_update_view(request):
     if request.method == 'POST':
@@ -263,6 +280,7 @@ def user_profile_update_view(request):
 
     return render(request, 'rmp_bd_app/profile_update.html', {'user_update_form': user_update_form})
 
+
 ''' 
 Creator: Mis Champa        Branch: Multicountry 2
 /search/?q=Professor name
@@ -270,6 +288,8 @@ Request made whenever User will search any professor name
 collect countyry database from maxmind company. Here is the link bellow
 https://www.maxmind.com/en/geoip2-country-database
  '''
+
+
 # Create SearchResultView function to filter Professor name based on IP address
 class SearchProfessorsResultsView(ListView):
     model = Professor
@@ -282,34 +302,37 @@ class SearchProfessorsResultsView(ListView):
         # if user click checkbox, engine will search professor first and last name by globally
         if 'search globally' in search_globally:
             professor_list = Professor.objects.filter(
-                    Q(first_name__icontains=query)|Q(last_name__icontains = query))
+                Q(first_name__icontains=query) | Q(last_name__icontains=query))
         # checks if the user is authenticated
         elif self.request.user.is_authenticated:
             current_user = User.objects.get(id=self.request.user.id)
             user_profile = StudentProfile.objects.get(user_id=current_user.id)
-            #getting user ip address
+            # getting user ip address
             user_ip = user_profile.ip_address
             # if user ip address is "0.0.0.0" ----> search by first or last name
             if user_ip == "0.0.0.0":
                 professor_list = Professor.objects.filter(
-                    Q(first_name__icontains=query)|Q(last_name__icontains = query))
+                    Q(first_name__icontains=query) | Q(last_name__icontains=query))
             # if user ip address is not "0.0.0.0" ----> search by first or last name in the associated country
             else:
                 g = GeoIP2()
                 country = g.country_code(user_ip)
                 professor_list = Professor.objects.filter(
-                                (Q(first_name__icontains = query) | Q(last_name__icontains = query)),
-                                current_university__country=country
-                            )
+                    (Q(first_name__icontains=query) | Q(last_name__icontains=query)),
+                    current_university__country=country
+                )
         else:
             professor_list = Professor.objects.filter(
                 Q(first_name__icontains=query) | Q(last_name__icontains=query))
 
         return professor_list
+
+
 def signedIn():
     user = authenticate(True)
     if user is not None:
         return user
+
 
 def thumbsUpDownIP(request, buttonPress):
     # Matt - psuedo code for the button action for like and dislike button
@@ -322,4 +345,3 @@ def thumbsUpDownIP(request, buttonPress):
         # if the ip is none --> attempt to recall the function to retrieve the IP
         elif retrieveIP(request) is None:
             thumbsUpDownIP(request, buttonPress)
-
