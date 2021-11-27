3a3f3049398c57704e27a34d5e11fb3b8091ccff
hschafer370 <90478800+hschafer370@users.noreply.github.com>
2021-11-27 10:36:10 -0500

    reformatting

--- rmp_bd/settings.py
+++ rmp_bd/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = '=607_q^=7f+2se*p!ethade(sse@ro@xhtw4iw01^0dxfz&c^%'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -44,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.gis.geoip2',
 ]
 GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
-#GEOIP_PATH =ROOT_PATH + '/geoip'
-                        #'django.contrib.gis.geoip2.base.py'
+# GEOIP_PATH =ROOT_PATH + '/geoip'
+# 'django.contrib.gis.geoip2.base.py'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -88,7 +87,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -107,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -120,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
