98daedba6e7695b422a1798f534f48e62a07ae95
Meghna Benoy <55854095+mbenoy@users.noreply.github.com>
2021-12-08 09:52:17 -0500

    Update forms.py

--- rmp_bd_app/forms.py
+++ rmp_bd_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import Textarea
 
 
 from .models import University, Department, Professor, StudentProfile, ProfessorProfile, Review, Course, ReviewTag
@@ -52,31 +53,11 @@ class CourseForm(forms.ModelForm):
         model = Course
         fields = ['course_number', 'course_title']
 
+
 class ReviewForm(forms.ModelForm):
-    # tags = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple, queryset=Review.objects.all())
+    is_online = forms.BooleanField(required=False)
     class Meta:
         model = Review
-        fields = [#'tags',  # 'course_number', 'course_title',
-                  'grade', 'semester', 'year',
-                  'mad_text', 'sad_text', 'glad_text', 'tags',
-                  'difficulty_level', 'professor_score', 'is_online', 'is_textbook',
-                  'is_credit', 'addTag']
-        labels = {
-            # 'course_title': 'Course Title',
-            # 'course_number': 'Course Code',
-            'grade': 'Grade Received',
-            'semester': 'Semester Taken',
-            'year': 'Year Taken',
-            'is_online': 'Online',
-            'mad_text': 'Mad: (e.g., things that you wish were not there)',
-            'sad_text': 'Sad: (e.g., things that you would change)',
-            'glad_text': 'Glad: (e.g., things you will want to keep for future students)',
-            'difficulty_level': 'Level of Difficulty',
-            'professor_score': 'Rate Your Professor',
-            'tags': 'Choose Your Tags',
-            'is_online': 'Did you take this class online?',
-            'is_textbook': 'Did you use the textbook(s) for this class?',
-            'is_credit': 'Did you take this class for credit?',
-            'addTag': 'Suggest New Tags'
-        }
+        fields = ['mad_text', 'sad_text', 'glad_text', 'difficulty_level', 'professor_score', 'grade',
+                  'is_online', 'tags', 'year', 'is_credit', 'newtag']
 
